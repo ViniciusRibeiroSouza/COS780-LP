e0efbe5c2af8a2f6d12efdcf12c902ee0dd1029e
ViniciusRibeiro <vinirsz@hotmail.com>
2022-06-12 14:48:37 -0300

    Feat: Added StandardForm linear programming class.

--- SimplexSolver/simplex.py
+++ SimplexSolver/simplex.py
@@ -1,101 +1,22 @@
 import numpy as np
 
-from SimplexSolver.utils import get_matrix_by_list_index
+from SimplexSolver.statandardFormLP import StandardFormLP
 
-
-class SimplexPrimal:
-    def __init__(self, linear_coefficients_matrix,
-                 linear_constraints_vector,
-                 linear_cost_vector):
-        self.reached_solution = False
-        self.slack_variables = None
-        self.current_cost_vector = None
-        self.current_base_matrix = None
-        self.current_basic_solution = None
-        self.current_non_base_matrix = None
-        self.current_inv_base_matrix = None
-        self.current_base_cost_vector = None
-        self.current_base_index_vector = None
-        self.current_constraints_vector = None
-        self.linear_cost_vector_expanded = None
-        self.current_coefficients_matrix = None
-        self.current_non_base_index_vector = None
-        self.linear_coefficients_matrix_expanded = None
-        self.linear_coefficients_matrix = np.array(linear_coefficients_matrix)
-        self.linear_constraints_vector = np.array(linear_constraints_vector)
-        self.linear_cost_vector = np.array(linear_cost_vector)
-        self.check()
-
-    def check(self):
-        invalid_vector_index, _ = np.where(self.linear_constraints_vector < 0)
-        if len(invalid_vector_index) > 0:
-            raise ValueError("All values in the constraints vector must be grater than or equal to 0.")
-
-    def frame_side_problem(self):
-        self.slack_variables = np.eye(self.linear_constraints_vector.shape[0])
-        self.linear_coefficients_matrix_expanded = np.column_stack(self.linear_coefficients_matrix,
-                                                                   self.slack_variables)
-        cost_vector_expanded = np.zeros(shape=(1, self.linear_coefficients_matrix.shape[1]))
-        slack_vector_expanded = np.ones(shape=(1, self.slack_variables.shape[1]))
-        self.linear_cost_vector_expanded = np.concatenate((cost_vector_expanded, slack_vector_expanded))
-
-    def init_solver(self):
-        self.reached_solution = False
-        self.current_base_matrix = None
-        self.current_basic_solution = None
-        self.current_non_base_matrix = None
-        self.current_base_cost_vector = None
-        self.current_base_index_vector = None
-        self.current_non_base_index_vector = None
-        self.current_cost_vector = self.linear_cost_vector
-        self.current_constraints_vector = self.linear_constraints_vector
-        self.current_coefficients_matrix = self.linear_coefficients_matrix
-
-    def solve(self, side_problem=False):
-        self.init_solver()
-        if side_problem:
-            self.current_cost_vector = self.linear_cost_vector_expanded
-            self.current_coefficients_matrix = self.linear_coefficients_matrix_expanded
-        while self.reached_solution:
-            self.current_base_matrix = self.get_current_base_matrix()
-            self.current_non_base_matrix = self.get_current_non_base_matrix()
-            self.current_basic_solution = self.calculate_current_basic_solution()
-            self.check_viability()
-            index_base_in, index_base_out, viability = self.get_base_change_indexes()
-            if not viability:
-                pass
-            self.update_current_inv_base_matrix(base_change_in=index_base_in,
-                                                base_change_out=index_base_out)
-            self.update_base_and_non_base_index_vector()
-            self.check_is_optimal()
-
-    def get_current_base_matrix(self):
-        return get_matrix_by_list_index(self.current_coefficients_matrix, self.current_base_index_vector)
-
-    def get_current_non_base_matrix(self):
-        return get_matrix_by_list_index(self.current_coefficients_matrix, self.current_non_base_index_vector)
-
-    def calculate_current_basic_solution(self):
-        self.get_current_inv_base_matrix(base_change=None)
-        return np.dot(self.current_inv_base_matrix, self.current_base_matrix)
-
-    def get_current_inv_base_matrix(self, base_change):
-        pass
-
-    def check_viability(self):
-        pass
-
-    def get_reduced_cost(self):
-        return np.array([])
-
-    def get_base_change_indexes(self):
-        return _, _, _
-
-    def update_base_and_non_base_index_vector(self):
-        pass
-
-    def update_current_inv_base_matrix(self, base_change_in, base_change_out):
-        pass
-
-    def check_is_optimal(self):
-        pass
+A = [[-2, 3, -10, 0],
+     [3, 2, 0, 15]]
+B = [[9],
+     [12]]
+C = [[2],
+     [1],
+     [0],
+     [0]]
+signs = ['>', '>', '>']
+directions = ['=', '=']
+model = StandardFormLP(a_matrix=np.array(A),
+                       b_matrix=np.array(B),
+                       c_matrix=np.array(C),
+                       max_or_min='min',
+                       signs=signs,
+                       directions=directions)
+var = model.get_simplex_phase_1_form()
+print(var)
---
+++ SimplexSolver/statandardFormLP.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+
+class StandardFormLP:
+    # Todo adjust documentation and variables behavior.
+    def __init__(self, a_matrix, b_matrix, c_matrix, signs=None, directions=None, max_or_min=None, standard_obj='max'):
+        """
+        Constructs a StandardFormLP from a Generic LP.
+            max c^T x (can also be min, if standard_obj='min')
+            s.t. Ax = b
+            x >= 0
+         """
+        assert max_or_min == 'max' or max_or_min == 'min'
+        self.original_objective = max_or_min
+        self.vars = {i: str(i) for i in range(a_matrix.shape[1])}
+        self.standard_form_obj = standard_obj
+        self.slacks_variables = []
+
+        # Variables of a Standard Form LP.
+        self.A = a_matrix
+        self.b = b_matrix
+        self.c = c_matrix if max_or_min == standard_obj else -1 * c_matrix
+        self._fix_var_domains(signs)
+        self._add_slack_vars(directions)
+
+    def _fix_var_domains(self, domain_restrictions):
+        """
+        Alters the A and c matrix so that all variables have domain x >= 0.
+        """
+        # Todo: Adjust problem if variables are free
+        for i, variable_restriction in enumerate(domain_restrictions):
+            if variable_restriction == '<':
+                self.c[i] *= -1
+                self.A[:, i] *= -1
+                self.vars[i] = '-' + self.vars[i]
+            elif variable_restriction == '=':
+                # We must cancel column value if Xi=0
+                self.c = np.append(self.c, -1 * self.c[i])
+                transposed_col = np.array([-1 * self.A[:, i]])
+                self.A = np.append(self.A, transposed_col.T, 1)
+                self.vars[i] = self.vars[i] + '-' + str(self.A.shape[1] - 1)
+
+    def _add_slack_vars(self, restriction_symbols):
+        """
+        Adds slack variables to transform inequalities to equalities.
+        """
+        for i, restriction_symbol in enumerate(restriction_symbols):
+            rows = self.A.shape[0]
+            if restriction_symbol == '>':
+                self.A = np.append(self.A, -1 * np.zeros(shape=[rows, 1]), 1)
+                self.A[i, -1] = -1
+                self.c = np.append(self.c, 0)
+                self.slacks_variables.append(self.A.shape[1] - 1)
+            elif restriction_symbol == '<':
+                self.A = np.append(self.A, 1 * np.zeros(shape=[rows, 1]), 1)
+                self.A[i, -1] = 1
+                self.c = np.append(self.c, 0)
+                self.slacks_variables.append(self.A.shape[1] - 1)
+
+    def get_simplex_phase_1_form(self):
+        """
+        Generates a table for Phase 1 of the simplex algorithm.
+        Adds artificial variables as needed.
+        """
+        artificial_variables = []
+        bfs = []
+        np_array_b = np.array(self.b)
+        rows, cols = self.A.shape
+        num_artificial = min(rows, cols)
+        np_array_a = np.array(self.A)
+        for row, col in enumerate(self.slacks_variables):
+            if np_array_a[row, col] == -1 and np_array_b[row] < 0:
+                np_array_a[row] = -1 * np_array_a[row]
+                np_array_b[row] = -1 * np_array_b[row]
+                num_artificial -= 1
+                bfs.append((row, col))
+            elif np_array_a[row, col] == 1 and np_array_b[row] > 0:
+                bfs.append((row, col))
+                num_artificial -= 1
+        np_array_a = np.append(np_array_a, np.zeros(shape=[rows, num_artificial]), 1)
+        # Add artificial variables
+        rows, cols = np_array_a.shape
+        bfs_rows = set(map(lambda x: x[0], bfs))
+        artificial_val = 0
+        for row_index in range(rows):
+            if row_index in bfs_rows:
+                continue
+            column_index = cols - num_artificial + artificial_val
+            artificial_variables.append((row_index, column_index))
+            bfs.append((row_index, column_index))
+            if np_array_a[row_index, -1] < 0:
+                np_array_a[row_index, :] = -1 * np_array_a[row_index, :]
+            np_array_a[row_index, column_index] = 1
+            artificial_val += 1
+        return np.column_stack((np_array_a, np_array_b)), artificial_variables, bfs
+
+    def are_dependent_constraints(self):
+        number_of_rows, number_of_columns = self.A.shape
+        if number_of_rows <= number_of_columns and number_of_rows != np.linalg.matrix_rank(self.A):
+            return True
+        return False
+
+    def convert_variables(self, x):
+        """Given a solution to the standard LP x, return the values of the original variables"""
+        assert len(x) == self.A.shape[1]
+        ans = np.zeros(len(self.vars))
+        for i in range(len(self.vars)):
+            standard_vars = self.vars[i].split('-')
+            if standard_vars[0] != '':
+                ans[i] += x[int(standard_vars[0])]
+            if len(standard_vars) > 1:
+                ans[i] -= x[int(standard_vars[1])]
+        assert len(ans) == len(self.vars)
+        return ans
+
+    def convert_objective(self, obj):
+        """Given an objective value to the standard LP obj, return the values"""
+        return obj if self.original_objective == self.standard_form_obj else -1 * obj
